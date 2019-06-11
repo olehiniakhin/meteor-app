df13fb09b6a5bf0608b5b4b437b6966b80c51442
olehiniakhin <inyakhin.oleh@protonmail.com>
2019-06-11 21:02:14 +0300

    Add simple todo list.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import React, { Component } from 'react';
+import Task from './Task'
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
-);
+export default class App extends Component {
+    getTasks = () => {
+        return [
+            { _id: 1, text: 'This is task 1' },
+            { _id: 2, text: 'This is task 2' },
+            { _id: 3, text: 'This is task 3' }
+        ]
+    }
 
-export default App;
+    renderTasks = () => {
+        return this.getTasks().map((task) => (
+            <Task key={task.id} task={task} />
+        ))
+    }
+
+    render = () => {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
+
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
+        )
+    }
+}
